1b36db8dfea8dceb8b7152df4cd94309fe538b6e
qintao <qintao@qt.(none)>
2014-10-24 17:20:01 +0800

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+  	@base_title="Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
 
     #assert_select the code above checks for the presence of a <title> tag containing the string “Home | Ruby on Rails Tutorial Sample App”
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -18,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
   end 
+
+  test "should get contact" do 
+  	get :contact
+  	assert_response :success
+  	assert_select "title","Contact | #{@base_title}"
+  end
 end
